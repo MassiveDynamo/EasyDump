954635ee7390e4d32d8e9e9f3f38b8542d42cb68
MassiveDynamo <gertgregers@outlook.com>
2025-02-14 17:14:28 +0100

    Add project files.

---
+++ EasyTest/Test1.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+
+namespace EasyTest;
+
+[TestClass]
+public sealed class Test1
+{
+    [DllImport("Kernel32", EntryPoint = "GetCurrentThreadId", ExactSpelling = true)]
+    public static extern Int32 GetCurrentWin32ThreadId();
+
+    [TestMethod, Timeout(1000)]
+    public void TestMethod1()
+    {
+        int threadId = Thread.CurrentThread.ManagedThreadId;
+        int unmanagedThreadId = GetCurrentWin32ThreadId();
+
+        Console.WriteLine($"Managed Thread Id {threadId:x}");
+        Console.WriteLine($"Unmanaged Thread Id {unmanagedThreadId:x}");
+        Thread.Sleep(5000);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Build a configuration object from command line
+        IConfiguration config = new ConfigurationBuilder()
+            .AddCommandLine(args)
+            .Build();
+
+        // Read configuration values
+        Console.WriteLine($"InputPath: {config["InputPath"]}");
+        Console.WriteLine($"OutputPath: {config["OutputPath"]}");
+    }
+}
