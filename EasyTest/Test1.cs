9b127e757869bc8e1125cf27cdc55f9515e1c2e6
MassiveDynamo <gertgregers@outlook.com>
2025-02-14 17:26:11 +0100

    Trying to write to the test output

--- EasyTest/Test1.cs
+++ EasyTest/Test1.cs
@@ -8,14 +8,26 @@ public sealed class Test1
     [DllImport("Kernel32", EntryPoint = "GetCurrentThreadId", ExactSpelling = true)]
     public static extern Int32 GetCurrentWin32ThreadId();
 
+    public required TestContext testContextInstance;
+
+    /// <summary>
+    /// Gets or sets the test context which provides
+    /// information about and functionality for the current test run.
+    /// </summary>
+    public TestContext TestContext
+    {
+        get { return testContextInstance; }
+        set { testContextInstance = value; }
+    }
+
     [TestMethod, Timeout(1000)]
     public void TestMethod1()
     {
         int threadId = Thread.CurrentThread.ManagedThreadId;
         int unmanagedThreadId = GetCurrentWin32ThreadId();
 
-        Console.WriteLine($"Managed Thread Id {threadId:x}");
-        Console.WriteLine($"Unmanaged Thread Id {unmanagedThreadId:x}");
+        TestContext.WriteLine($"Managed Thread Id 0x{threadId:x}");
+        TestContext.WriteLine($"Unmanaged Thread Id 0x{unmanagedThreadId:x}");
         Thread.Sleep(5000);
     }
 }
